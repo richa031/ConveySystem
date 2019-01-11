66b54f9ab24c24a608eab3ce3e488b73a744e514
richa031 <richahp031@gmail.com>
2019-01-11 22:53:31 +0530

    modal

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Fa,Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, FormInline, Dropdown, DropdownToggle, DropdownMenu,  DropdownItem, MDBBtn } from "mdbreact";
+import { Fa,Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, FormInline, Dropdown, DropdownToggle, DropdownMenu,  DropdownItem, MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput } from "mdbreact";
 import {Modal, ModalHeader, ModalBody} from 'reactstrap';
 
 class NavbarPage extends React.Component {
@@ -90,9 +90,50 @@ class NavbarPage extends React.Component {
             </NavbarNav>
           </Collapse>
         </Navbar>
+
+
       <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal} >
-        <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
-      </Modal>
+      <ModalBody toggle={this.toggleModal}>
+      <MDBContainer>
+      <MDBRow>
+        <MDBCol md="12">
+          <MDBCard>
+            <div className="header pt-3 grey lighten-2">
+              <MDBRow className="d-flex justify-content-start">
+                <h3 className="deep-grey-text mt-3 mb-4 pb-1 mx-5">
+                  Log in
+                </h3>
+              </MDBRow>
+            </div>
+            <MDBCardBody className="mx-4 mt-4">
+              <MDBInput label="Your email" group type="text" validate />
+              <MDBInput
+                label="Your password"
+                group
+                type="password"
+                validate
+                containerClass="mb-0"
+              />
+              <p className="font-small grey-text d-flex justify-content-end">
+                Forgot
+                <a href="#!" className="dark-grey-text font-weight-bold ml-1">
+                  Password?
+                </a>
+              </p>
+              <div className="text-center mb-4 mt-5">
+                <MDBBtn color="default" type="button" className="btn-block z-depth-2">Log in</MDBBtn>
+              </div>
+              <p className="font-small grey-text d-flex justify-content-center">
+                Don't have an account?
+                <a href="#!" className="dark-grey-text font-weight-bold ml-1">Sign up</a>
+              </p>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+    </ModalBody>
+    </Modal>
       </div>
     );
   }

213fae2f2c28a61ea2991e049dc4265e4bff74a9
richa031 <richahp031@gmail.com>
2019-01-18 17:32:54 +0530

    google authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,8 @@
 import React, { Component } from 'react';
-import PrimarySearchAppBar from './navbar';
-import { Jumbotron, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import NavbarPage from './Nav';
 import CarouselPage from './Carousel';
 import JumbotronPage from './Jumbotron';
 
 class Header extends Component{
-
-    constructor(props){
-        super(props);
-        this.state={
-            isNavOpen: false
-           
-        };
-        this.toggleNav = this.toggleNav.bind(this);
-        
-    }
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
-    }
-
-    
 
     render(){
         return(//using React fragments
